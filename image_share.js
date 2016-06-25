1667a42a0ed11eee82716be5ea44c42591c9465f
Alexa Anderson <alexa.anderson@outlook.com>
2016-06-25 17:04:58 -0500

    Add infinite scroll

--- image_share.js
+++ image_share.js
@@ -3,9 +3,23 @@
 Images = new Mongo.Collection("images");
 
 if (Meteor.isClient){
-  // Passing in an object with a property images
-  // which contains image data array of objects
-  // Template.images.helpers({images:img_data});
+  
+  // Used to set up infinite scroll
+  Session.set("imageLimit", 8);
+  lastScrollTop = 0;
+  $(window).scroll(function(event){
+    // Tests if we are near the bottom of the window.
+    if ($(window).scrollTop() + $(window).height() > $(document).height()-100){
+      // Where are we on the page
+      var scrollTop = $(this).scrollTop();
+      // Test if I am going down
+      if (scrollTop > lastScrollTop){
+        Session.set("imageLimit", Session.get("imageLimit") + 4);
+      }
+
+      lastScrollTop = scrollTop;
+    }
+  });
 
   // Function available through accounts packages
   Accounts.ui.config({
@@ -24,7 +38,8 @@ if (Meteor.isClient){
       else {
         // find: {} -> all of them
         //       rating: -1  -> reverse (high to low)
-        return Images.find({}, {sort:{createdOn: -1, rating:-1}});
+        //       options object: sort/limit etc
+        return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")});
       }
     },
     filtering_images: function(){
